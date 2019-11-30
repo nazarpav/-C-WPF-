b17b7d59e3ab03a42a0b543d4b322291b8298332
павлюк назар <hasab.k18@gmail.com>
2019-11-30 23:31:51 +0200

    30.11.2019

---
+++ ClassWork/18.11.2019/18.11.2019/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _18._11._2019
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string num1 = String.Empty, num2= String.Empty;
+        List<string> ListNum = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListNum.Add("");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_15(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_16(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_17(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
