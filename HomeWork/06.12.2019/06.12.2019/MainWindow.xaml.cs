4663a97e8b052ce4e3b7ead470338efdf457b376
павлюк назар <hasab.k18@gmail.com>
2019-12-06 23:53:21 +0200

    06.12.2019

---
+++ HomeWork/06.12.2019/06.12.2019/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace _06._12._2019
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timerVideoTime;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timerVideoTime = new DispatcherTimer();
+            timerVideoTime.Interval = TimeSpan.FromSeconds(0.1);
+            timerVideoTime.Tick += new EventHandler(timer_Tick);
+            MediaElement.Stop();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MediaElement.Play();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MediaElement.Pause();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MediaElement.
+        }
+    }
+}
